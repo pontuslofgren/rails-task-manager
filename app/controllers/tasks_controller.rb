137bc7ec0036a284930b081ebb91b6a473e5e968
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-11-14 15:20:08 +0000

    User can view all and individual tasks, add, edit and delete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :set_tasks
+  before_action :set_task, only: %i[show edit update destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+  end
+
+  def update
+  @task.update(task_params)
+  redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path(@tasks)
+  end
+
+  private
+
+  def set_tasks
+    @tasks = Task.all
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Add new task
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  patch 'tasks/:id', to: 'tasks#update'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Delete a task
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  # List all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # Display task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+
 end
