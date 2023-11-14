278eba185c10fc80b22974d46728084edc12084d
Pontus Lofgren <pontus.loefgren@gmail.com>
2023-11-14 15:35:00 +0000

    Refactored routes to use resources and not individual routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-  # Add new task
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # # Add new task
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  # Edit a task
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks/:id', to: 'tasks#update'
+  # # Edit a task
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks/:id', to: 'tasks#update'
 
-  # Delete a task
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # Delete a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
-  # List all tasks
-  get 'tasks', to: 'tasks#index'
+  # # List all tasks
+  # get 'tasks', to: 'tasks#index'
 
-  # Display task
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # # Display task
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
 
-
+  resources :tasks
 end
